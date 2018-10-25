cc6487b52342e502ca78261e1bf778702a264d69
HyphenDev@HYP-Linux01 <htakahashi@hyphenio.com.au>
2018-10-26 07:17:03 +1000

    Updated the navbar

--- resources/views/www/index.blade.php
+++ resources/views/www/index.blade.php
@@ -14,34 +14,36 @@
 </head>
 <body>
   <header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="min-height: 120px;">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="min-height: 120px;">
       <a class="navbar-brand" href="#"><img src="/images/logo.png"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">        
-        <div style="position: absolute; left: 40%;">
+        <div style="position: absolute;left: 25%;width: 50%;">
           <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <input class="form-control mr-sm-2" type="text" style="width: 80%;" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
           </form>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
         </div>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <div style="background-color: white; padding: 5px 10px; border-radius: 5px 0 0 5px; height: 40px;">
-          <i class="fa fa-2x fa-shopping-cart"></i>
-        </div>
-        <div style="padding: 10px 10px; border-radius: 0 5px 5px 0; background-color: red; height: 40px; color: white; font-weight: bold;">
-          CHECKOUT
+        <div style="position: absolute;right: 3%;">
+          <div style="background-color: white; padding: 5px 10px; border-radius: 5px 5px 0 0; height: 40px;">
+            <i class="fa fa-2x fa-shopping-cart"></i>
+          </div>
+          <div style="padding: 10px 10px; border-radius: 0 0 5px 5px; background-color: red; height: 40px; color: white; font-weight: bold;">
+            CHECKOUT
+          </div>
         </div>
       </div>
     </nav>
