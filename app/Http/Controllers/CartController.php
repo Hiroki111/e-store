fa5e3d9088e163bf86da19354b461c3341220771
HyphenDev@HYP-Linux01 <htakahashi@hyphenio.com.au>
2018-11-02 08:33:04 +1000

    Update CartController and CartTest

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,6 +11,27 @@ class CartController extends Controller
         return response()->json(session('cart'), 201);
     }
 
+    public function add()
+    {
+        $this->validate(request(), [
+            'type' => ['required'],
+            'qty'  => ['required', 'integer', 'min:0'],
+        ]);
+
+        $type = request('type');
+        $cart = session('cart');
+
+        if (empty($cart[$type][request('productId')])) {
+            $cart[$type][request('productId')] = 0;
+        }
+
+        $cart[$type][request('productId')] += request('qty');
+
+        session(['cart' => $cart]);
+
+        return response()->json($cart, 201);
+    }
+
     public function store(Request $request)
     {
         $this->validate(request(), [
@@ -18,11 +39,9 @@ class CartController extends Controller
             'qty'  => ['required', 'integer', 'min:0'],
         ]);
 
-        $cart = session('cart');
-        $type = request('type');
-        if ($type === 'product') {
-            $cart['products'][request('productId')] = request('qty');
-        }
+        $cart                              = session('cart');
+        $type                              = request('type');
+        $cart[$type][request('productId')] = request('qty');
 
         session(['cart' => $cart]);
 
--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -10,22 +10,60 @@ class CartTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function canAddItemToCart()
+    {
+        $this->assertEquals(session('cart'), null);
+
+        $this->post('/cart/add', [
+            'type'      => 'products',
+            'productId' => 1,
+            'qty'       => 1,
+        ]);
+
+        $expected = [
+            'products' => [
+                1 => 1,
+            ],
+        ];
+        $this->assertEquals(session('cart'), $expected);
+
+        $this->post('/cart/add', [
+            'type'      => 'products',
+            'productId' => 2,
+            'qty'       => 1,
+        ]);
+        $this->post('/cart/add', [
+            'type'      => 'products',
+            'productId' => 1,
+            'qty'       => 2,
+        ]);
+
+        $expected = [
+            'products' => [
+                1 => 3,
+                2 => 1,
+            ],
+        ];
+        $this->assertEquals(session('cart'), $expected);
+    }
+
+    /** @test */
     public function canUpdateCart()
     {
         $this->assertEquals(session('cart'), null);
 
         $this->post('/cart', [
-            'type'      => 'product',
+            'type'      => 'products',
             'productId' => 1,
             'qty'       => 3,
         ]);
         $this->post('/cart', [
-            'type'      => 'product',
+            'type'      => 'products',
             'productId' => 2,
             'qty'       => 1,
         ]);
         $this->post('/cart', [
-            'type'      => 'product',
+            'type'      => 'products',
             'productId' => 1,
             'qty'       => 0,
         ]);
@@ -52,7 +90,7 @@ class CartTest extends TestCase
         $this->assertEquals(session('cart'), null);
 
         $response = $this->json('post', '/cart', [
-            'type'      => 'product',
+            'type'      => 'products',
             'productId' => 1,
         ]);
         $response->assertStatus(422);
