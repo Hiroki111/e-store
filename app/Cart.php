8b5e94cbeeb41ac3dcf11c8a09fd6b03778a375d
HyphenDev@HYP-Linux01 <htakahashi@hyphenio.com.au>
2018-12-20 08:25:17 +1000

    Created cart class Added a test for cart

---
+++ app/Cart.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+use App\Bundle;
+use App\Product;
+use ArrayIterator;
+use IteratorAggregate;
+
+class Cart implements IteratorAggregate
+{
+    private $count;
+    private $items;
+
+    public function __construct($items)
+    {
+        $this->items = $items;
+        $this->count = collect($this->items)->map(function ($items) {
+            return array_reduce($items, function ($total, $item) {return $total + $item;});
+        })->reduce(function ($total, $qty) {
+            return $total + $qty;
+        });
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function getItems()
+    {
+        return collect($this->items)->map(function ($items, $type) {
+            return collect($items)->map(function ($qty, $hashedId) use ($type) {
+                $class = ($type === 'products') ? Product::class : Bundle::class;
+                $item  = $class::find(decode_hash($hashedId));
+                return (object) [
+                    'src'         => $item->src,
+                    'name'        => $item->name,
+                    'price'       => $item->price,
+                    'qty'         => $qty,
+                    'total_price' => number_format((double) $item->price * $qty, 2),
+                ];
+            })->values()->all();
+        })->collapse()->all();
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this->getItems());
+    }
+
+    public function getTotalPrice()
+    {
+        $total = array_reduce($this->getItems(), function ($total, $item) {
+            return $total + (double) $item->total_price;
+        });
+        return number_format($total, 2);
+    }
+}
--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests;
 
+use App\Bundle;
+use App\Cart;
+use App\Product;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -123,5 +126,46 @@ class CartTest extends TestCase
         ]);
         $response->assertStatus(422);
         $this->assertEquals(session('cart'), null);
+    }
+
+    /** @test */
+    public function canGetItemsAndTotalPrice()
+    {
+        $this->assertEquals(session('cart'), null);
+
+        $product1 = factory(Product::class)->create();
+        $product2 = factory(Product::class)->create();
+        $product3 = factory(Product::class)->create();
+        $product4 = factory(Product::class)->create();
+        $bundle   = factory(Bundle::class)->create();
+        $bundle->products()->attach([$product2->id, $product3->id, $product4->id]);
+
+        $this->post('/cart/add', [
+            'type'   => 'products',
+            'itemId' => $product1->hashed_id,
+            'qty'    => 2,
+        ]);
+        $this->post('/cart/add', [
+            'type'   => 'bundles',
+            'itemId' => $bundle->hashed_id,
+            'qty'    => 1,
+        ]);
+
+        $cart = new Cart(session('cart'));
+
+        $cartItems = $cart->getItems();
+        collect($cartItems)->zip([[$product1, $product1], [$bundle]])->each(function ($set) {
+            $cartItem   = $set[0];
+            $actualItem = collect($set[1])->first();
+            $qty        = sizeof($set[1]);
+
+            $this->assertEquals($cartItem->name, $actualItem->name);
+            $this->assertEquals($cartItem->price, $actualItem->price);
+            $this->assertEquals($cartItem->qty, $qty);
+            $this->assertEquals($cartItem->total_price, number_format($actualItem->price * $qty, 2));
+        });
+        $this->assertEquals(sizeof($cartItems), 2);
+
+        $this->assertEquals($cart->getTotalPrice(), number_format($product1->price * 2 + $bundle->price, 2));
     }
 }
