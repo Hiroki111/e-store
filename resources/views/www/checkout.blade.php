fb5a6219214060456d7a38aabef5490398b43eb3
Hiroki Takahashi <mitsuke1116@gmail.com>
2019-01-20 15:22:22 +1000

    Updated custom.css Updated the style of checkout page

--- resources/views/www/checkout.blade.php
+++ resources/views/www/checkout.blade.php
@@ -7,14 +7,14 @@
 <div class="container">
 	<div class="row">
 		<div class="col-10 offset-md-1">
-			<h3 class="font-weight-bold font-arial" style="margin: 30px 0 10px;">View Cart ({{$cart->count}}@if($cart->count > 1) items @else item @endif)</h3>
-			<table class="table">
+			<h3 class="font-weight-bold font-arial" style="margin: 20px 0 0 0;">View Cart ({{$cart->count}}@if($cart->count > 1) items @else item @endif)</h3>
+			<table id="checkout-table" class="table">
 				<thead>
 					<tr style="border-bottom: 2px solid black;">
-						<th style="border: none; font-size: 20px;">Your Items</th>
-						<th style="border: none; font-size: 20px;"></th>
-						<th style="border: none; font-size: 20px;">Quantity</th>
-						<th style="border: none; font-size: 20px; padding-right: 0;
+						<th class="font-arial">Your Items</th>
+						<th class="font-arial"></th>
+						<th class="font-arial">Quantity</th>
+						<th class="font-arial" style="padding-right: 0;
 						float: right;">Price</th>
 					</tr>
 				</thead>
@@ -22,17 +22,19 @@
 					@foreach($cart as $i => $item)
 					<tr>
 						<td style="width: 10%">
-							<img src="{{$item->src}}" style="max-height: 100px;">
+							<img src="{{$item->src}}" style="max-height: 100px; display: block; margin: 0 auto;">
 						</td>
 						<td>
-							<h5 class="font-weight-bold" style="color: red;">{{$item->name}}</h5>
-							<p>Each ${{$item->price}}</p>
+							<div>
+								<h5 class="font-weight-bold font-arial" style="color: red;">{{$item->name}}</h5>
+								<p style="margin: 0;">Each ${{$item->price}}</p>
+							</div>
 						</td>
 						<td>
-							<p class="font-weight-bold">{{$item->qty}}</p>
+							<div><p class="font-weight-bold">{{$item->qty}}</p></div>
 						</td>
 						<td style="width: 10%; padding-right: 0;">
-							<p class="font-weight-bold pull-right">${{$item->total_price}}</p>
+							<div><p class="font-weight-bold pull-right">${{$item->total_price}}</p></div>
 						</td>
 					</tr>
 					@endforeach
@@ -41,7 +43,7 @@
 		</div>
 		<hr>
 		<div class="col-3 offset-8">
-			<h3 class="font-weight-bold pull-right">Total ${{$cart->getTotalPrice()}}</h3>
+			<h3 class="font-weight-bold font-arial pull-right">Total ${{$cart->getTotalPrice()}}</h3>
 		</div>
 		<div class="col-3 offset-8" style="padding-bottom: 10px;">
 			<span class="pull-right"><a href="/">Continue Shopping</a> OR <button class="btn font-weight-bold" style="color: white; background-color: red;">Checkout</button></span>
