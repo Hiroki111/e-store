293cca67ca93dd0e1f2355624a3db88a473fd9b2
Hiroki 111 <mitsuke1116@gmail.com>
2019-05-04 15:31:53 +0200

    Added delivery page and animation scss

---
+++ app/Http/Controllers/DeliveryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DeliveryController extends Controller
+{
+    public function index()
+    {
+        return view('www.delivery');
+    }
+}
---
+++ resources/views/www/delivery.blade.php
@@ -0,0 +1,21 @@
+@extends('www.layout.master')
+@section('title', 'Delivery')
+@section('content')
+<div class="delivery">
+    <div class="delivery__header">
+        <div>
+            <i class="fa fa-glass" aria-hidden="true"></i>
+            <i class="fa fa-beer" aria-hidden="true"></i>
+            <i class="fa fa-gift" aria-hidden="true"></i>
+            <i class="fa fa-glass" aria-hidden="true"></i>
+            <i class="fa fa-beer" aria-hidden="true"></i>
+            <i class="fa fa-gift" aria-hidden="true"></i>
+            <i class="fa fa-glass" aria-hidden="true"></i>
+            <i class="fa fa-beer" aria-hidden="true"></i>
+            <i class="fa fa-gift" aria-hidden="true"></i>
+            <i class="fa fa-truck" aria-hidden="true"></i>
+        </div>
+        <p>delivery to suit you</p>
+    </div>
+</div>
+@endsection
