d259661db57f6eb94c533d7e9f0a0c1edd6eff7a
Hiroki 111 <mitsuke1116@gmail.com>
2019-05-09 06:48:16 +0200

    Added scss for the top page Updated the styles of the top page

--- resources/views/www/index.blade.php
+++ resources/views/www/index.blade.php
@@ -1,80 +1,72 @@
 @extends('www.layout.master')
-
 @section('title', 'Top')
-
 @section('content')
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    @foreach($slides as $i => $slide)
-    <div class="carousel-item @if($i === 0) active @endif" style="background-image: url({{$slide->src}});">
-      <div class="container">
-        <div class="carousel-caption text-left">
-          <h1 class="outlined-text">{{$slide->title}}</h1>
-          <p class="outlined-text">{{$slide->description}}</p>
-          <p><a class="btn btn-lg btn-primary" href="/product-type/{{$slide->product_id}}" role="button">MORE</a></p>
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        @foreach($slides as $i => $slide)
+        <div class="carousel-item @if($i === 0) active @endif" style="background-image: url({{$slide->src}});">
+            <div class="container">
+                <div class="carousel-caption text-left">
+                    <h1 class="outlined-text">{{$slide->title}}</h1>
+                    <p class="outlined-text">{{$slide->description}}</p>
+                    <p><a class="btn btn-lg btn-primary" href="/product-type/{{$slide->product_id}}" role="button">MORE</a></p>
+                </div>
+            </div>
         </div>
-      </div>
+        @endforeach
     </div>
-    @endforeach
-  </div>
-  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
 <div class="container marketing">
-  <div class="row">
-    <div class="col-lg-4">
-      <h2><i class="fa fa-beer" aria-hidden="true"></i></i> Click & Collect</h2>
-      <div style="min-height: 60px">
-        <p style="margin: 0;">Order online and pick up in store in as little as 60 minutes.</p>
-        <p style="margin: 0;"></p>
-      </div>
-      <p><a style="font-weight: bold; color: red; font-family: 'Arial',Helvetica,sans-serif;" href="/click-and-collect">VIEW DETAILS &raquo;</a></p>
+    <div class="delivery-order-info">
+        <div class="delivery-order-info-item">
+            <h2><i class="fa fa-beer" aria-hidden="true"></i></i> Click & Collect</h2>
+            <div class="delivery-order-info-description">
+                <p>Order online and pick up in store in as little as 60 minutes.</p>
+            </div>
+            <p class="font-arial"><a href="/click-and-collect">VIEW DETAILS &raquo;</a></p>
+        </div>
+        <div class="delivery-order-info-item">
+            <h2><i class="fa fa-truck fa-flip-horizontal" aria-hidden="true"></i> Delivery Info</h2>
+            <div class="delivery-order-info-description">
+                <p>FREE domestic delivery on all wine</p>
+            </div>
+            <p class="font-arial"><a href="/delivery">VIEW DETAILS &raquo;</a></p>
+        </div>
+        <div class="delivery-order-info-item">
+            <h2><i class="fa fa-shopping-cart" aria-hidden="true"></i> Order &amp; Payment</h2>
+            <div class="delivery-order-info-description">
+                <p>Online, Phone, In Store</p>
+            </div>
+            <p class="font-arial"><a href="/orderandpayment">VIEW DETAILS &raquo;</a></p>
+        </div>
     </div>
-    <div class="col-lg-4">
-      <h2><i class="fa fa-truck fa-flip-horizontal" aria-hidden="true"></i> Delivery Info</h2>
-      <div style="min-height: 60px">
-        <p style="margin: 0;">FREE domestic delivery on all wine</p>
-        <p style="margin: 0;">FREE international delivery on any order over $150</p>
-      </div>
-      <p><a style="font-weight: bold; color: red; font-family: 'Arial',Helvetica,sans-serif;" href="/delivery">VIEW DETAILS &raquo;</a></p>
+    <hr>
+    <h1 class="font-arial">Special Bundles</h1>
+    <div class="bundle-container">
+        @foreach($recommendedBundles as $bundle)
+        @include('www.util.productpanel', ['item' => $bundle, 'type' => 'bundle'])
+        @endforeach
     </div>
-    <div class="col-lg-4">
-      <h2><i class="fa fa-shopping-cart" aria-hidden="true"></i> Order &amp; Payment</h2>
-      <div style="min-height: 60px">
-        <p style="margin: 0;">Online, Phone, In Store</p>
-      </div>
-      <p><a style="font-weight: bold; color: red; font-family: 'Arial',Helvetica,sans-serif;" href="/orderandpayment">VIEW DETAILS &raquo;</a></p>
+    <hr>
+    <h1 class="font-arial">Top Picks</h1>
+    <div class="product-container">
+        @foreach($recommendedProducts as $product)
+        @include('www.util.productpanel', ['item' => $product, 'type' => 'product'])
+        @endforeach
     </div>
-  </div>
-
-  <hr class="" style="margin: 30px 0;">
-  <h1 style="font-family: Arial, Helvetica, sans-serif; font-weight: bold;">Special Bundles</h1>
-  <div class="row">
-    @foreach($recommendedBundles as $bundle)
-    @include('www.util.productpanel', ['item' => $bundle, 'type' => 'bundle'])
-    @endforeach
-  </div>
-
-  <hr class="" style="margin: 30px 0;">
-  <h1 style="font-family: Arial, Helvetica, sans-serif; font-weight: bold;">Top Picks</h1>
-  <div class="row">
-    @foreach($recommendedProducts as $product)
-    @include('www.util.productpanel', ['item' => $product, 'type' => 'product'])
-    @endforeach
-  </div>
-
-  @include('www.layout.addeditem')
+    @include('www.layout.addeditem')
 </div>
-
 @endsection
--- resources/views/www/util/productpanel.blade.php
+++ resources/views/www/util/productpanel.blade.php
@@ -1,38 +1,38 @@
-<div class="col-md-3">
-  <div class="card mb-3 shadow-sm">
-    <a href="/{{$type}}/{{$item->hashed_id}}">
-      <div class="card-img-frame">
-        <img class="card-img-top" src="{{$item->src}}">
-      </div>
-    </a>
-    <div class="card-body text-center">
-      <h5 class="card-text" style="font-weight: bold;">{{$item->name}}</h5>
-      <div class=" flex-wrapper flex-wrapper-first">
-        <div class="product-tile-price-big" style="color: #D2232A; display: flex;">
-          <div class="price-bundle-new">
-            <span class="price" style="font-size: 22px; font-weight: 900; vertical-align: top;">
-              <span class="currency" style="font-size: 12px;">$</span>
-              {{$item->dollars}}
-              <span class="cents" style="font-size: 12px;">.{{$item->cents}}</span>
-            </span>
-          </div>
-          <div class="price-des" style="font-size: 12px; margin-left: 5px; margin-top: 7px;">Each</div>
+<div class="card-wrapper">
+    <div class="card mb-3 shadow-sm">
+        <a href="/{{$type}}/{{$item->hashed_id}}">
+            <div class="card-img-frame">
+                <img class="card-img-top" src="{{$item->src}}">
+            </div>
+        </a>
+        <div class="card-body text-center">
+            <h5 class="card-text">{{$item->name}}</h5>
+            <div class="flex-wrapper flex-wrapper-first">
+                <div class="product-tile-price-big" style="color: #D2232A; display: flex;">
+                    <div class="price-bundle-new">
+                        <span class="price" style="font-size: 22px; font-weight: 900; vertical-align: top;">
+                            <span class="currency" style="font-size: 12px;">$</span>
+                            {{$item->dollars}}
+                            <span class="cents" style="font-size: 12px;">.{{$item->cents}}</span>
+                        </span>
+                    </div>
+                    <div class="price-des" style="font-size: 12px; margin-left: 5px; margin-top: 7px;">Each</div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <button type="button" class="btn btn-sm btn-outline-secondary update-qty-btn" data-update-qty="-1" data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}">-</button>
+                    </div>
+                    <input type="number" id="{{$type}}-{{$item->hashed_id}}" min="1" max="50" value="1" style="text-align: center;" class="form-control">
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-sm btn-outline-secondary update-qty-btn" data-update-qty="1" data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}">+</button>
+                    </div>
+                </div>
+            </div>
+            <div class="input-group">
+                <button data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}" data-item-src="{{$item->src}}" type="button" class="btn add-item add-to-cart-button">Add to Cart</button>
+            </div>
         </div>
-      </div>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <button type="button" class="btn btn-sm btn-outline-secondary update-qty-btn" data-update-qty="-1" data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}">-</button>
-          </div>
-          <input type="number" id="{{$type}}-{{$item->hashed_id}}" min="1" max="50" value="1" style="text-align: center;" class="form-control">
-          <div class="input-group-append">
-            <button type="button" class="btn btn-sm btn-outline-secondary update-qty-btn" data-update-qty="1" data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}" >+</button>
-          </div>
-        </div>
-      </div>
-      <div class="input-group">
-        <button data-item-type="{{$type}}" data-item-id="{{$item->hashed_id}}" data-item-src="{{$item->src}}" type="button" class="btn add-item add-to-cart-button" >Add to Cart</button>
-      </div>
     </div>
-  </div>
 </div>
