5c7b77e3d5b1ef0609caad1499bf6529529508bd
Hiroki 111 <mitsuke1116@gmail.com>
2019-05-18 14:06:29 +0200

    Updated meta data

--- resources/views/www/layout/master.blade.php
+++ resources/views/www/layout/master.blade.php
@@ -3,10 +3,22 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="An imagenary liquor store">
+  <meta name="description" content="An imaginary liquor store">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title') | Hiroki's Liquor Store</title>
+
+  <meta property="og:description" content="Australia's number 1 online drinks shop. Hard to get & collectable spirits, beer, wine barware and more. (NOTE: This is an imaginary shop, so you won't be able to buy anything)">
+  <meta property="og:url" content="https://store.hiroki-t.com/">
+  <meta property="og:image" content="https://store.hiroki-t.com/images/slides/1.jpeg">
+  <meta property="og:title" content="Hiroki's Liquor Store">
+  <meta property="og:site_name" content="Hiroki's Liquor Store">
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@takahas779">
+  <meta name="twitter:title" content="Hiroki's Liquor Store">
+  <meta name="twitter:description" content="Australia's number 1 online drinks shop. Hard to get & collectable spirits, beer, wine barware and more. (NOTE: This is an imaginary shop, so you won't be able to buy anything)">
+  <meta name="twitter:image" content="https://store.hiroki-t.com/images/slides/1.jpeg">
 
   <link rel="stylesheet" type="text/css" href="/css/app.css">
   <link rel="stylesheet" type="text/css" href="/css/main.css">
